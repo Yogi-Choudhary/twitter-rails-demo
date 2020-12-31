5d30bfacc20e442985999a87b20031170ca40c21
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2020-12-31 10:53:24 +0530

    modify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+mount_uploader :images, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :tweeets, dependent: :destroy
 end
